6f0c4650f59a2cb1e2ab31e3c42010a49ffcf494
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2024-10-14 18:13:06 +0000

    javascript questions

--- Problem/main.js
+++ Problem/main.js
@@ -15,3 +15,12 @@ ob.greet();   //output is null
 const arr = [1,2,3];
 const sum = arr.reduce((acc, curr)=> acc+curr,0);
 console.log(sum)
+
+
+
+
+
+console.log(NaN === NaN);
+
+let name = 'John';
+console.log(`Hello, ${name}!`);
