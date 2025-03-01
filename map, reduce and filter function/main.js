d37dbece28b0ddc4c8147f2f8c06de5f25308b0b
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2025-03-01 12:34:48 +0000

    for loop

---
+++ map, reduce and filter function/main.js
@@ -0,0 +1,29 @@
+const companies = [
+    {name: "Google", category:"Product Based", start: 1998, end: 2004},
+    {name: "Facebook", category:"Product Based", start: 2004, end: 2010},
+    {name: "Amazon", category:"Product Based", start: 2000, end: 2006},
+    {name: "Microsoft", category:"Product Based", start: 1996, end: 2002},
+    {name: "Apple", category:"Product Based", start: 1991, end: 1997},
+    {name: "Infosys", category:"Service Based", start: 2001, end: 2007},
+    {name: "TCS", category:"Service Based", start: 2003, end: 2009},
+    {name: "Wipro", category:"Service Based", start: 2005, end: 2011},
+    {name: "Accenture", category:"Service Based", start: 2007, end: 2013},
+    {name: "IBM", category:"Service Based", start: 2009, end: 2015},
+    {name: "PAYTM", category:"Product Based", start: 2011, end: 2017},
+    {name: "OLA", category:"Product Based", start: 2013, end: 2019},
+    {name: "SWIGGY", category:"Product Based", start: 2015, end: 2021},
+    {name: "ZOMATO", category:"Product Based", start: 2017, end: 2023},
+]
+
+
+
+
+// for
+
+for (let i  = 0; i <companies.length; i++){
+    //  console.log(companies[i]);
+    //  console.log(companies[i].name);
+    // console.log(companies[i].category);
+    // console.log(companies[i].start);
+    // console.log(companies[i].end);
+} 
